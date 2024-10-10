a534a5e5c2ec1acf4bb3fd4b5e0b254bec07c2fa
KoLorca <100674270+Lorinc-Vagi@users.noreply.github.com>
2024-10-10 13:09:35 +0200

    initialize

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Kosar from './lib/Kosar.svelte';
+</script>
+
+<main>
+  <div class="card">
+    <Kosar termekek={[
+      {nev:'Sajt',ar:2000,db:1},
+      {nev:'Kenyér',ar:2000,db:1}
+    ]}/>
+    
+  </div>
+</main>
+
+<style>
+</style>
---
+++ src/lib/Kosar.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let termekek
+</script>
+
+<h1>Kosár</h1>
+<hr>
+{#each termekek as i}
+  <b>{i.nev} </b>{i.ar}ft - {i.db} db
+  <br>
+{/each}
+<style>
+  b{
+    color: blue;
+  }
+</style>
