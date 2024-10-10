d5ef49601f6afe568ffa6e5afc2c36e5f0421c50
KoLorca <100674270+Lorinc-Vagi@users.noreply.github.com>
2024-10-10 13:28:30 +0200

    fix3

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base:'/mai/',
+  base:'/oraiMunka2024.10.10_2/',
   build:{
     outDir:'docs',
     emptyOutDir:true
